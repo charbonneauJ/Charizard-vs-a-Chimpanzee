da49dca863e79fac0175d2a37491ca2c2cc91817
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-06-06 20:02:35 -0300

    debug?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
-//todo store to local storage
+//todo get user input of which animal and pokemon
 
-//todo get from local storage
+//todo get stats and pics from local storage
 
-//todo display animal cards
+//todo display animal and pokemon cards
+
+let animal = {
+  name: "panther",
+  height: "28 inches",
+  weight: "140 lbs",
+  speed: "50 mph",
+};
+const animals = [animal];
+
+function createAnimalCard(animals) {
+  const animalCard = $("<div>")
+    .addClass("card animal-card")
+    .attr("data-animal-id", animals.id);
+  const cardHeader = $("<div>").addClass("card-header h3").text(animals.name);
+  const cardBody = $("<div>").addClass("card-body");
+  const cardStats = $("<div>")
+    .addClass("card-stats")
+    .text(animals.height, animals.weight, animals.speed);
+}
+
+const animalList = $("#animals");
+animalList.append(createAnimalCard(animals));
